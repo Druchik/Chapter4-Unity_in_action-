0d3e5c2817b94bdf5055331467570e0370bcfa0f
Andrey Shaklein <as1854@yandex.ru>
2017-10-02 19:35:03 +0300

    Initial commit

---
+++ Assets/Scripts/Lidar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lidar : MonoBehaviour
+{
+    void Update()
+    {
+        //Луч находится в том же положении и нацеливается в том же направлении, что и объект
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        //Испущенный луч заполняет информацией переменную, на которую имеется ссылка
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Загружаем в сообщение координаты точки, в которую попал луч
+            Debug.Log("Hit " + hit.point);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+
+    //Создаем сериализованные переменные лидара и игрока
+    [SerializeField] private GameObject _lidarPrefab;
+    private GameObject _lidar;
+    [SerializeField] private GameObject _playerPrefab;
+    private GameObject _player;
+    void Update()
+    {
+        //Если игрока нет на сцене, то создаем его
+        if (_player == null)
+        {
+            _player = Instantiate(_playerPrefab) as GameObject;
+            _player.transform.position = new Vector3(0, 1, -11);
+            //Каждый раз поворачиваем игрока в новом направлении
+            float angle = Random.Range(0, 360);
+            _player.transform.Rotate(0, angle, 0);
+            Camera.main.fieldOfView = Random.Range(10, 60);
+        }
+
+        //Если лидара нет, то содаем, каждый раз поворачивая его в новом направлении
+        if (_lidar == null)
+        {
+            _lidar = Instantiate(_lidarPrefab) as GameObject;
+            _lidar.transform.position = new Vector3(0, 1, 0);
+            float angle = Random.Range(0, 360);
+            _lidar.transform.Rotate(0, angle, 0);
+        }
+    }
+} 
+
